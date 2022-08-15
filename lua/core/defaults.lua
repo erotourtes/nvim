b900bf2dde008d73a31daf6fd1f9e2da76070494
sirmax <erotourtes@gmail.com>
2022-08-15 11:03:23 +0300

    updated core

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -1,4 +1,6 @@
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -18,13 +20,68 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 10
---vim.opt.signcolumn = "yes"
---vim.opt.isfname:append("@-@")
 
--- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
--- delays and poor user experience.
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-
 vim.opt.shell = "fish"
+
+vim.opt.laststatus = 3 -- Set global status line
+
+
+local function set_hi_groups()
+  vim.cmd([[highlight WinSeparator guibg=None]]) -- Remove borders for window separators
+  vim.cmd([[highlight SignColumn guibg=None]]) -- Remove background from signs column
+end
+set_hi_groups()
+
+
+-- Add position to jumplist if moving more than 5 lines up or down
+vim.keymap.set("n", "j", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'j' : 'j']], { expr = true })
+vim.keymap.set("n", "k", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'k' : 'k']], { expr = true })
+
+
+-- Don't auto comment new lines
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=c fo-=r fo-=o",
+})
+
+
+-- Disable some default plugins
+local default_plugins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "python3_provider",
+  "python_provider",
+  "node_provider",
+  "ruby_provider",
+  "perl_provider",
+  "tutor",
+  "rplugin",
+  "syntax",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  -- "ftplugin",
+}
+
+for _, plugin in pairs(default_plugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,16 @@
 local set = vim.keymap.set
 vim.g.mapleader = " "
 
+
 set("v", "J", ":m '>+1<CR>gv=gv")
 set("v", "K", ":m '<-2<CR>gv=gv")
+
 
 set("n", "n", "nzzzv")
 set("n", "N", "Nzzzv")
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
+
 
 set("v", "<leader>y", "\"+y")
 set("v", "<leader>y", "\"+y")
@@ -19,3 +22,18 @@ set("n", "<leader>d", "\"_d")
 set("v", "<leader>d", "\"_d")
 
 set("n", "x", "\"_x")
+
+
+set("n", "tn", "<cmd>tabnew<CR>") -- Create new tab
+set({ "n", "i" }, "<C-L>", "<cmd>tabnext<CR>") -- Switch to right tab
+set({ "n", "i" }, "<C-H>", "<cmd>tabprev<CR>") -- Switch to left tab
+set("n", "tc", "<cmd>tabclose<CR>") -- Close current tab
+set("n", "tac", "<cmd>tabo<CR>") -- Close all tabs
+
+
+set("n", "vsp", ":vsplit<Return><C-w>w")
+set("n", "sp", ":split<Return><C-w>j")
+set("n", "<leader>h", "<C-w>h")
+set("n", "<leader>j", "<C-w>j")
+set("n", "<leader>k", "<C-w>k")
+set("n", "<leader>l", "<C-w>l")
