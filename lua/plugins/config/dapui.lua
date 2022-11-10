8869f3d2dafb0c27cce3059a221222035665670c
sirmax <erotourtes@gmail.com>
2022-11-10 15:03:06 +0200

    feat(dap): js

--- lua/plugins/config/dapui.lua
+++ lua/plugins/config/dapui.lua
@@ -2,7 +2,30 @@ local present, dapui = pcall(require, "dapui")
 if not present then return end
 
 local dap = require("dap")
-dapui.setup()
+-- dapui.setup()
+dapui.setup({
+ layouts = {
+   {
+     elements = {
+      -- Elements can be strings or table with id and size keys.
+      "watches",
+      { id = "scopes", size = 0.5 },
+      "breakpoints",
+    },
+      size = 40, -- 40 columns
+      position = "left",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+        "stacks",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
+  },
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
@@ -13,3 +36,5 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+
