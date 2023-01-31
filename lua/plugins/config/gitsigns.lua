529d28a34ddbe9410fff34f73bd408c519253a02
Max Sir <erotourtes@gmail.com>
2023-01-31 18:01:55 +0200

    fix(gitsighns): normal left line

--- lua/plugins/config/gitsigns.lua
+++ lua/plugins/config/gitsigns.lua
@@ -6,29 +6,41 @@ vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<cr>")
 vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<cr>zz")
 vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<cr>zz")
 
-local ic = require("ui.icons").ui
+local icons = require("ui.icons").ui
 
 local options = {
   signs = {
-    add = { hl = "GitSignsAdd", text = ic["U258E"], numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = ic["U258E"], numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = ic["U25B8"], numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = ic["U25B8"], numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    add = {
+      hl = "GitSignsAdd",
+      text = icons.block,
+      numhl = "GitSignsAddNr",
+      linehl = "GitSignsAddLn",
+    },
+    change = {
+      hl = "GitSignsChange",
+      text = icons.block,
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn",
+    },
+    delete = {
+      hl = "GitSignsDelete",
+      text = icons.triangle,
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
+    topdelete = {
+      hl = "GitSignsDelete",
+      text = icons.triangle,
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
     changedelete = {
       hl = "GitSignsChange",
-      text = ic["U258E"],
+      text = icons.block,
       numhl = "GitSignsChangeNr",
       linehl = "GitSignsChangeLn",
     },
   },
-  yadm = { enable = false },
-}
-
-local function set_hi_groups()
-  vim.cmd([[highlight GitSignsAdd guibg=None guifg=Green]])
-  vim.cmd([[highlight GitSignsChange guibg=None guifg=DarkCyan]])
-  vim.cmd([[highlight GitSignsDelete guibg=None guifg=Grey]])
-end
+  yadm = { enable = false },}
 
 gitsigns.setup(options)
-set_hi_groups()
