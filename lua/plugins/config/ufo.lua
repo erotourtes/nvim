8a0d6ee8c2b8f3ba661fcff2cc619a271868ef8e
Max Sir <erotourtes@gmail.com>
2023-03-07 14:59:36 +0200

    feat: ufo

---
+++ lua/plugins/config/ufo.lua
@@ -0,0 +1,18 @@
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zO', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zC', require('ufo').closeAllFolds)
+vim.keymap.set('n', 'zp', require('ufo').goPreviousStartFold)
+vim.keymap.set('n', 'zn', require('ufo').goNextClosedFold)
+vim.keymap.set('n', 'zj', require('ufo').peekFoldedLinesUnderCursor)
+
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
+
