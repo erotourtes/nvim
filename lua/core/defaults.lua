aa5030a95db8fd69586f2c2b4f3d06b0c6069a87
erotourtes <erotourtes@gmail.com>
2023-12-14 11:14:37 +0200

    fix: ua layout in neovim

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -37,63 +37,75 @@ vim.opt.laststatus = 3 -- Set global status line
 vim.keymap.set("n", "j", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'j' : 'j']], { expr = true })
 vim.keymap.set("n", "k", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'k' : 'k']], { expr = true })
 
-autocmd("TextYankPost", {
-  pattern = "*",
-  group = vim.api.nvim_create_augroup("TextYankPost", { clear = true }),
-  callback = function()
-    local mode = vim.api.nvim_get_mode()["mode"]
+-- Ukrainian keyboard layout (through langmap) https://gist.github.com/bebyx/09307c0d9bdd381add8e9fd623a827d3
+--[[
+set langmap=йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ї],фa,іs,вd,аf,пg,рh,оj,лk,дl,ж\\;,
+  \є',ґ\\,яz,чx,сc,мv,иb,тn,ьm,б\\,,ю.,,ЙQ,ЦW,УE,КR,ЕT,НY,НY,ГU,ШI,ЩO,ЗP,Х{,Ї},ФA,
+  \ІS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\\:,Є\\",Ґ\|,ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б\\<,Ю>,№#
+]]
 
-    -- Only highlight in normal mode. not in visual
-    if mode == "no" then
-      vim.highlight.on_yank({
-        higroup = "Visual",
-        timeout = 75,
-      })
-    end
-  end,
+vim.api.nvim_set_option(
+	"langmap",
+	"йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ї],фa,іs,вd,аf,пg,рh,оj,лk,дl,ж\\;,є',ґ\\,яz,чx,сc,мv,иb,тn,ьm,б\\,,ю.,,ЙQ,ЦW,УE,КR,ЕT,НY,НY,ГU,ШI,ЩO,ЗP,Х{,Ї},ФA,ІS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\\:,Є\",Ґ\\|,ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б\\<,Ю>,№#"
+)
+
+autocmd("TextYankPost", {
+	pattern = "*",
+	group = vim.api.nvim_create_augroup("TextYankPost", { clear = true }),
+	callback = function()
+		local mode = vim.api.nvim_get_mode()["mode"]
+
+		-- Only highlight in normal mode. not in visual
+		if mode == "no" then
+			vim.highlight.on_yank({
+				higroup = "Visual",
+				timeout = 75,
+			})
+		end
+	end,
 })
 
 -- Don't auto comment new lines
 autocmd("BufEnter", {
-  pattern = "*",
-  command = "set fo-=c fo-=r fo-=o",
+	pattern = "*",
+	command = "set fo-=c fo-=r fo-=o",
 })
 
 -- Disable some default plugins
 local default_plugins = {
-  "2html_plugin",
-  "getscript",
-  "getscriptPlugin",
-  "gzip",
-  "logipat",
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
-  "matchit",
-  "tar",
-  "tarPlugin",
-  "rrhelper",
-  "spellfile_plugin",
-  "vimball",
-  "vimballPlugin",
-  "zip",
-  "zipPlugin",
-  "python3_provider",
-  "python_provider",
-  "node_provider",
-  "ruby_provider",
-  "perl_provider",
-  "tutor",
-  "rplugin",
-  "syntax",
-  "synmenu",
-  "optwin",
-  "compiler",
-  "bugreport",
-  -- "ftplugin",
+	"2html_plugin",
+	"getscript",
+	"getscriptPlugin",
+	"gzip",
+	"logipat",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"matchit",
+	"tar",
+	"tarPlugin",
+	"rrhelper",
+	"spellfile_plugin",
+	"vimball",
+	"vimballPlugin",
+	"zip",
+	"zipPlugin",
+	"python3_provider",
+	"python_provider",
+	"node_provider",
+	"ruby_provider",
+	"perl_provider",
+	"tutor",
+	"rplugin",
+	"syntax",
+	"synmenu",
+	"optwin",
+	"compiler",
+	"bugreport",
+	-- "ftplugin",
 }
 
 for _, plugin in pairs(default_plugins) do
-  vim.g["loaded_" .. plugin] = 1
+	vim.g["loaded_" .. plugin] = 1
 end
